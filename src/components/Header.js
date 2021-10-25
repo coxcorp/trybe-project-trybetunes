c0d0916fcd90bf369a371ec3fedbe1f9c93bd50b
djsilva <coxcorp@gmail.com>
2021-10-25 01:42:38 -0300

    requisito 04

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,9 +33,9 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         <nav>
-          <Link to="/search">Pesquisa</Link>
-          <Link to="/favorites">Favoritas</Link>
-          <Link to="/profile">Perfil</Link>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
         </nav>
         { loading ? <Loading /> : this.renderUserName() }
       </header>
