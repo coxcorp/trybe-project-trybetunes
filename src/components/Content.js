6444ab229c291974aa045e73d5b23d26683941bc
djsilva <coxcorp@gmail.com>
2021-10-25 02:17:46 -0300

    requisito 01 - correçao

--- src/components/Content.js
+++ src/components/Content.js
@@ -20,7 +20,7 @@ class Content extends React.Component {
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
           <Route exact path="/" component={ Login } />
-          <Route path="/profile" component={ Profile } />
+          <Route exact path="/profile" component={ Profile } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/search" component={ Search } />
           <Route path="*" component={ NotFound } />
